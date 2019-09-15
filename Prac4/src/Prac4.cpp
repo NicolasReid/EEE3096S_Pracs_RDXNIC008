0ab9a4a7db0b223f7ecb0206b29122671186c735
rdxnic008 <rdxnic008@myuct.ac.za>
2019-09-15 09:57:28 +0200

    Final edits and coments

--- Prac4/src/Prac4.cpp
+++ Prac4/src/Prac4.cpp
@@ -5,6 +5,8 @@
  *
  * Adapted for EEE3096S 2019 by Keegan Crankshaw
  *
+ * Further Edits by Nicolas Reid (RDXNIC008) and Mathew Hayes (HYSMAT002)
+ *
  * This file is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
