c87b704ff443434eaee605f341c378d870a1f164
hysmat <hysmat002@myuct.ac.za>
2019-10-14 18:41:16 +0200

    Button updates and Alarm

--- ClassProject/envLogger.py
+++ ClassProject/envLogger.py
@@ -14,7 +14,7 @@ import RPi.GPIO as GPIO  # for GPIO
 import time              # for time --> remove when rtc is working
 import threading         # for threads
 import datetime          # for timing
-import BlynkLib          # for blynk app
+#import BlynkLib          # for blynk app
 
 #---------------------------Global Variables----------------------------
 
@@ -22,8 +22,8 @@ IsGPIO = False
 IsSPI = False
 delay = 5
 logging = True
-Alarm = False
-blynk = BlynkLib.Blynk('QaSs_5koxPXKY8STWWwvX3Eqsdsi-1U3') # Initialize Blynk
+alarm = False
+#blynk = BlynkLib.Blynk('QaSs_5koxPXKY8STWWwvX3Eqsdsi-1U3') # Initialize Blynk
 
 # For ADC SPI
 firstByte = int('00000001',2)
@@ -40,7 +40,7 @@ dac = spidev.SpiDev()
 upperLimit = 2.65
 lowerLimit = 0.65
 AlarmString = " "
-lastAlarmTime = 0 
+lastAlarmTime = 0
 alarmBuffer = 3 #min
 # For time
 sysStart = datetime.datetime.now()
@@ -55,20 +55,30 @@ V_0 = 0.55  #Volts
 
 Vout = 0
 
+pwm = []
 #----------------------------functions----------------------------------
 def init():
-
     # init Gpio
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setwarnings(False)
+    #Inputs
+    GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP) # reset system time
+    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP) # decrement
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) # increment
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # start/stop
+    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # deactivate alarm
 
+     # Setup interupts for HW buttons
+    GPIO.add_event_detect(15, GPIO.FALLING, callback=increment, bouncetime=200)
+    GPIO.add_event_detect(18, GPIO.FALLING, callback=decrement, bouncetime=200)
+    GPIO.add_event_detect(14, GPIO.FALLING, callback=resetSysTime, bouncetime=200)
+    GPIO.add_event_detect(23, GPIO.FALLING, callback=stopStart, bouncetime=200)
+    GPIO.add_event_detect(24, GPIO.FALLING, callback=alarmDeactivate, bouncetime=200)
     # outputs
-    #GPIO.setup(chipSelectPins, GPIO.OUT)
-    #GPIO.output(chipSelectPins, 1) # set chip select pins hight because communication starts on a logic low.
-
+    GPIO.setup(12, GPIO.OUT)
+    alarmPwm = GPIO.PWM(12, 262)
+    pwm.append(alarmPwm)
+    IsGPIO = True
     # init SPI    adc = spidev.SpiDev()
     adc.open(0,1)
     dac.open(0,0)
@@ -78,11 +88,13 @@ def init():
     dataThread = threading.Thread(target = dataThreadFunction)
     mainThread = threading.Thread(target = mainThreadFunction)
     DACThread = threading.Thread(target = DACThreadFunction)
-    buttonThread = threading.Thread(target = buttonThreadFunction)
+    alarmThread = threading.Thread(target = alarmThreadFunction)
+    #buttonThread = threading.Thread(target = buttonThreadFunction)
     threads.append(mainThread)
     threads.append(DACThread)
     threads.append(dataThread)
-    threads.append(buttonThread)
+    threads.append(alarmThread)
+    #threads.append(buttonThread)
 
 
 def mainThreadFunction():
@@ -94,14 +106,17 @@ def mainThreadFunction():
             now = datetime.datetime.now()
             print('|{:^10}|{:^11}|   {:1.1f}V   |  {:2.0f}°C  |  {:^4.0f} |  {:1.2f}V  |   {}   |'.format(now.strftime("%H:%M:%S"), str(now-sysStart)[:-7], Data[1], Data[2],Data[3],Vout,AlarmString))
             print("+----------+-----------+----------+--------+-------+---------+-------+")
-            wait = time.time()
-            while((time.time()-wait) < delay):
-                pass
+    #wait = time.time()
+    #while((time.time()-wait) < delay):
+        #pass
+        time.sleep(delay)
 
 def dataThreadFunction():
     global sysSec
+    global logging
     while(1):
-        getADCData()
+        if logging:
+            getADCData()
         time.sleep(1)
 
 def DACThreadFunction():
@@ -109,31 +124,49 @@ def DACThreadFunction():
     global Vref
     global lastAlarmTime
     global AlarmString
+    global alarm
+    global logging
+    global upperLimit, lowerLimit
     while (1):
-        Vout = Data[1]/1023 * Data[3]
-        if(((Vout < lowerLimit) or (Vout > upperLimit)) and ((lastAlarmTime - time.time()) > (60*alarmBuffer) or not lastAlarmTime)):
-            print("Alarm")
-            Alarm = True;
-            AlarmString = "*"
-            lastAlarmTime = time.time()
-
-        Dn = int((Vout*2**10)/Vref)
-        Dn = Dn << 2
-        Dn = commandBits | Dn
-        upperByte = Dn >> 8
-        lowerByte = Dn & int('0000000011111111',2)
-        dac.xfer([upperByte,lowerByte],10000,16)
+        if logging:
+            Vout = Data[1]/1023 * Data[3]
+            if(((Vout < lowerLimit) or (Vout > upperLimit)) and ((time.time()-lastAlarmTime) > (60*alarmBuffer))):
+                AlarmString = "*"
+                lastAlarmTime = time.time()
+                alarm = True
+            Dn = int((Vout*2**10)/Vref)
+            Dn = Dn << 2
+            Dn = commandBits | Dn
+            upperByte = Dn >> 8
+            lowerByte = Dn & int('0000000011111111',2)
+            dac.xfer([upperByte,lowerByte],10000,16)
         time.sleep(1)
 
-def buttonThreadFunction():
-    # Register Virtual Pins
-    @blynk.VIRTUAL_WRITE(1)
-    def my_write_handler(value):
-        global delay
-        delay = int(value[0])
 
+def alarmThreadFunction():
+    global alarm
+    global pwm
+    duty = 50
     while(1):
-        blynk.run()
+        if(alarm):
+            pwm[0].start(duty)
+            time.sleep(0.5)
+            if (duty == 70):
+                duty = 50
+            else:
+                duty = 70
+        pwm[0].stop()
+        time.sleep(0.01)
+
+#def buttonThreadFunction():
+    # Register Virtual Pins
+ #   @blynk.VIRTUAL_WRITE(1)
+  #  def my_write_handler(value):
+    #    global delay
+   #     delay = int(value[0])
+
+   # while(1):
+    #    blynk.run()
 
 def convertToVoltage (ADC_Output ,Vref= 3.3):
     v = ((ADC_Output[1] & int('00000011',2)) << 8) + ADC_Output[2]
@@ -165,18 +198,24 @@ def getADCData():
 def decrement(channel):
     global delay
     switcher = {
+        1: 5,
         2: 1,
         5: 2,
     }
     delay = switcher.get(delay, delay)
+    print("|                           Delay is {:d}                               |".format(delay))
+    print("+----------+-----------+----------+--------+-------+---------+-------+")
 
 def increment(channel):
     global delay
     switcher = {
         1: 2,
         2: 5,
+        5: 1,
     }
     delay = switcher.get(delay, delay)
+    print("|                           Delay is {:d}                               |".format(delay))
+    print("+----------+-----------+----------+--------+-------+---------+-------+")
 
 def resetSysTime(channel):
     global sysStart
@@ -185,9 +224,33 @@ def resetSysTime(channel):
 def stopStart(channel):
     global logging
     logging = not logging
+    if logging:
+        #print("+----------+-----------+----------+--------+-------+---------+-------+")
+        print("|                              Starting                              |")
+        print("+----------+-----------+----------+--------+-------+---------+-------+")
+    else:
+        #print("+----------+-----------+----------+--------+-------+---------+-------+")
+        print("|                              Stopping                              |")
+        print("+----------+-----------+----------+--------+-------+---------+-------+")
+
+def alarmDeactivate(channel):
+    global alarm, AlarmString
+    if(alarm):
+        alarm = False
+        AlarmString = " "
+        #print("+----------+-----------+----------+--------+-------+---------+-------+")
+        print("|                           Alarm Dismissed                          |")
+        print("+----------+-----------+----------+--------+-------+---------+-------+")
+
+    else:
+        print("|                     Oi stop pressing buttons :(                    |")
+        print("+----------+-----------+----------+--------+-------+---------+-------+")
 
 def main():
     global Vout
+    global Alarm
+    global AlarmString
+    global lowerLiminit, upperLimit
     print("+====================================================================+")
     print("|                              Starting                              |")
     print("+----------+-----------+----------+--------+-------+---------+-------+")
@@ -196,15 +259,12 @@ def main():
     init()
     getADCData()
     Vout = Data[1]/1023 * Data[3]
+    if((Vout < lowerLimit) or (Vout > upperLimit)):
+          #print("Alarm")
+          Alarm = True;
+          AlarmString = "*"
+          lastAlarmTime = time.time()
 
-    # Setup interupts for HW buttons
-    GPIO.add_event_detect(15, GPIO.FALLING, callback=increment, bouncetime=130)
-    GPIO.add_event_detect(18, GPIO.FALLING, callback=decrement, bouncetime=130)
-    GPIO.add_event_detect(14, GPIO.FALLING, callback=resetSysTime, bouncetime=130)
-    GPIO.add_event_detect(23, GPIO.FALLING, callback=stopStart, bouncetime=130)
-
-
-    # Start threads
     for thread in threads:
         thread.start()
 
@@ -212,10 +272,9 @@ def main():
 
 
 def cleanUp():
-    if IsGPIO:
-        GPIO.cleanup()
-    if IsSPI:
-        acd.close()
+    GPIO.cleanup()
+    acd.close()
+    dac.cllose()
     for thread in threads():
         thread.join()
 
@@ -225,6 +284,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nExiting gracefully")
         cleanUp()
+        GPIO.cleanup()
     except Exception as e:
         cleanUp()
         print("Error: ")
